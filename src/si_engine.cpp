755d0f0151d73f0ff957cd1672100dfcd390b522
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-11-19 19:22:45 +0100

    Refactor size calculation using custom worker parameters

--- src/si_engine.cpp
+++ src/si_engine.cpp
@@ -11,6 +11,7 @@
 
 struct SizeInfoData {
     QString name {};
+    QString path {};
     bool isDir {false};
     qint64 bytes {0};
 };
@@ -28,92 +29,42 @@ int SpaceInspectorEngine::requestFolderSizeInfo(const QString& folder)
 {
     // This function calculates sizes of all folder contents and a
     // summary of the folder itself.
-    //
-    // Due to limitations in Engine's disk space worker implementation,
-    // we have to pass results around as QStringList objects. The
-    // final conversion is done in the main thread which is suboptimal.
 
-    return runDiskSpaceWorker([&](int handle, QStringList result){
-        // This code is run on the main thread.
-        // It converts the calculated sizes into structured objects
-        // that can be passed to QML.
+    return runDiskSpaceWorker([&](int handle, QVariant resultVariant){
+        const auto& result = resultVariant.value<QPair<FolderSizeStatusInfo, QVariantList>>();
+        emit folderSizeInfoReady(handle, result.first, result.second);
+    }, [folder]() -> QVariant {
+        QFileInfo folderInfo(folder);
 
-        FolderSizeStatusInfo status(
-            result[0].isEmpty() ? false : true,
-            QFileInfo(result[0]).fileName(),
-            QFileInfo(result[0]).absoluteFilePath(),
-            result[1].toLongLong(),
-            result[2].toLongLong(),
-            result[3]
-        );
-
-        QVariantList info;
-        info.reserve(result.length() / 4);
-
-        for (auto i = 4; i < result.length(); i += 4) {
-            info.append(QVariant::fromValue<SizeInfo>({
-                result[i+0].split('/').last(),
-                result[i+0],
-                result[i+1] == QStringLiteral("folder"),
-                result[i+2].toLongLong(),
-                result[i+3],
-            }));
-        }
-
-        emit folderSizeInfoReady(handle, status, info);
-    }, [folder]() -> QStringList {
-        // This code is run in the worker thread.
-        // We calculate sizes and return results as a string list.
-
-        /**
-         * Info fields:
-         *   1. processed folder: not empty on success, empty on failure
-         *   2. how many folders were counted? (e.g. "451")
-         *   3. how many files were counted? (e.g. "1984")
-         *   4. combined human readable disk usage, e.g. "5.0 GiB",
-         *      falls back to "-"
-         *
-         *   5...: groups of four lines for each folder entry:
-         *     a. name without path, e.g. "Documents"
-         *     b. type, either "file" or "folder"
-         *     c. disk usage in bytes
-         *          - must be converted back to int for further processing
-         *          - falls back to 0 if incalculable
-         *          - guaranteed to be >= 0
-         *     d. human readable disk usage, e.g. "2.1 GiB", falls back to "-"
-         *
-         * The result list always has 4*n fields.
-         * All fields are empty if the data could not be determined.
-         */
-
-        if (folder.isEmpty() || !QFileInfo(folder).isDir()) {
-            return {{}, {}, {}, {}};
+        if (folder.isEmpty() || !folderInfo.isDir()) {
+            return QVariant::fromValue(
+                QPair<FolderSizeStatusInfo, QVariantList>{{}, {}});
         }
 
         int files = 0;
         int dirs = 0;
-        qint64 bytes = QFileInfo(folder).size();
+        qint64 bytes = folderInfo.size();
 
-        auto processSubdir = [&bytes](const QString& subdir) -> SizeInfoData {
-            qint64 subdirBytes = 0;
-            QDirIterator it(subdir,
-                QDir::AllEntries |
-                QDir::System |
+        auto processSubdir = [](const QFileInfo& subdir, qint64& fileBytes) -> void {
+            QDirIterator it(subdir.absoluteFilePath(),
+                // QDir::AllEntries | -- excluded: devices
+                // QDir::System | -- excluded: broken symlinks
+                QDir::Dirs |
+                QDir::Files |
+                QDir::NoSymLinks | // they don't take space
                 QDir::NoDotAndDotDot |
                 QDir::Hidden,
                 // list entries recursively
                 QDirIterator::Subdirectories);
 
             while (!it.next().isEmpty()) {
-                const auto& info = it.fileInfo();
-                subdirBytes += info.size();
+                fileBytes += it.fileInfo().size();
             }
-
-            subdirBytes += QFileInfo(subdir).size();
-            bytes += subdirBytes;
-            return {QFileInfo(subdir).absoluteFilePath(), true, subdirBytes};
         };
 
+        // Absolutely all entries are included in the base
+        // listing so they show up in the file list. Special
+        // files are excluded when processing folder contents.
         auto entriesCount = QDir(folder,
             QLatin1String(""),
             QDir::NoSort,
@@ -135,17 +86,25 @@ int SpaceInspectorEngine::requestFolderSizeInfo(const QString& folder)
             // list only immediate entries
             QDirIterator::NoIteratorFlags);
 
+        qint64 fileBytes = 0;
         while(!it.next().isEmpty()) {
             const auto& info = it.fileInfo();
+            fileBytes = info.size();
 
-            if (info.isDir()) {
+            if (info.isDir() && !info.isSymLink()) {
                 ++dirs;
-                items.append(processSubdir(info.absoluteFilePath()));
+                processSubdir(info, fileBytes);
             } else {
                 ++files;
-                bytes += info.size();
-                items.append({info.absoluteFilePath(), false, info.size()});
             }
+
+            bytes += fileBytes;
+            items.append({
+                info.fileName(),
+                info.absoluteFilePath(),
+                info.isDir(),
+                fileBytes
+            });
         }
 
         std::sort(items.begin(), items.begin()+items.count(),
@@ -153,27 +112,31 @@ int SpaceInspectorEngine::requestFolderSizeInfo(const QString& folder)
             return a.bytes > b.bytes;
         });
 
-        QStringList result {
-            folder,
-            QStringLiteral("0"),
-            QStringLiteral("0"),
-            QStringLiteral("-"),
-        };
-        result.reserve((entriesCount + 1) * 4);
+        FolderSizeStatusInfo status(
+            true,
+            folderInfo.fileName(),
+            folderInfo.absoluteFilePath(),
+            dirs,
+            files,
+            filesizeToString(bytes)
+        );
+
+        QVariantList info;
+        info.reserve(entriesCount);
 
         for (const auto& i : items) {
-            result.append({
+            info.append(QVariant::fromValue<SizeInfo>(SizeInfo{
                 i.name,
-                i.isDir ? QStringLiteral("folder") :
-                          QStringLiteral("file"),
-                QString::number(i.bytes),
-                filesizeToString(i.bytes),
-            });
+                i.path,
+                i.isDir,
+                i.bytes,
+                filesizeToString(i.bytes)
+            }));
         }
 
-        result[1] = QString::number(dirs);
-        result[2] = QString::number(files);
-        result[3] = filesizeToString(bytes);
-        return result;
+        return QVariant::fromValue(
+            QPair<FolderSizeStatusInfo, QVariantList>{
+                status, info
+        });
     });
 }
